262629ecb5d7cd71e1b44dfb192479aeeea6d0df
Huska, Matthew <HuskaM@rki.de>
2025-07-15 09:41:05 +0200

    Don't leave miniforge installer in the user's home directory

--- install/terminal/app-miniforge.sh
+++ install/terminal/app-miniforge.sh
@@ -1,3 +1,6 @@
-curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
-bash Miniforge3-$(uname)-$(uname -m).sh -b -f
-
+DOWNLOAD_URL="https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$(uname)-$(uname -m).sh"
+DOWNLOAD_DIR="$(mktemp -d)"
+# Clean up installer when the script finishes
+trap 'rm -rf -- "$DOWNLOAD_DIR"' EXIT
+wget -O "$DOWNLOAD_DIR/miniforge.sh" "$DOWNLOAD_URL"
+bash "$DOWNLOAD_DIR/miniforge.sh" -b -f
